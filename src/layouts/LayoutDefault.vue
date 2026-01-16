e5c4d426abec2a29df2b8fb5bc9fa9887567f026
Mateuss18 <mateusdfl18@gmail.com>
2026-01-15 22:40:35 -0300

    feat(layout): add default layout component with header and router view

---
+++ src/layouts/LayoutDefault.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <AppHeader />
+
+    <RouterView />
+  </q-layout>
+</template>
+
+<script setup>
+import AppHeader from 'src/components/AppHeader.vue'
+</script>
+
+<style lang="scss">
+@import './LayoutDefault.scss';
+</style>
