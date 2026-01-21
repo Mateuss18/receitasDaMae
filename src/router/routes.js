8642c84621b8a6b84f1c4622677168cd5ae06a07
Mateuss18 <mateusdfl18@gmail.com>
2026-01-21 00:15:50 -0300

    feat(router): add named routes for recipe operations

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,27 +5,11 @@ const routes = [
     component: () => import('src/layouts/LayoutDefault.vue'),
     children: [
       { path: '', component: () => import('pages/HomePage.vue') },
-      { path: '/create-recipe', component: () => import('pages/RecipeCreate.vue') }
+      { name: 'create', path: '/create-recipe', component: () => import('pages/RecipeCreate.vue') },
+      { name: 'detail', path: '/recipe/:id', component: () => import('pages/RecipeDetail.vue') },
+      { name: 'update', path: '/update-recipe/:id', component: () => import('pages/RecipeUpdate.vue') }
     ]
   },
-  // {
-  //   name: 'create',
-  //   path: '/create-recipe',
-  //   component: () => import('pages/RecipeCreate.vue')
-  // },
-  {
-    name: 'edit',
-    path: '/edit/:id',
-    component: () => import('pages/RecipeEdit.vue')
-  },
-  {
-    name: 'detail',
-    path: '/recipe/:id',
-    component: () => import('pages/RecipeDetail.vue')
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
