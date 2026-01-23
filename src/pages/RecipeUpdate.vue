b067d235b11d8d089f6f52de37a32e5ac046f6aa
Mateuss18 <mateusdfl18@gmail.com>
2026-01-22 23:52:41 -0300

    refactor(recipe-update): rename edit to update for consistency

--- src/pages/RecipeUpdate.vue
+++ src/pages/RecipeUpdate.vue
@@ -3,8 +3,8 @@
     <RecipeForm
       title="Editar receita"
       :recipe-values="selectedRecipe"
-      mode="edit"
-      @submit="handleSubmitEdit"
+      mode="update"
+      @submit="handleSubmitUpdate"
     />
   </div>
   <div v-else>Receita não existe</div>
@@ -12,28 +12,31 @@
 
 <script setup>
 import { onMounted, ref, computed } from 'vue'
+
 import { useRoute, useRouter } from 'vue-router'
+
 import { getAll, saveAll } from '../services/recipesStorage'
+
 import RecipeForm from 'src/components/RecipeForm.vue'
 
 const dataRecipes = ref([])
 const router = useRouter()
 const route = useRoute()
-const recipeToEditID = String(route.params.id)
+const recipeToUpdateID = String(route.params.id)
 
 onMounted(() => {
   dataRecipes.value = getAll()
 })
 
 const selectedRecipe = computed(() => {
-  return dataRecipes.value.find((recipeItem) => recipeItem.id === recipeToEditID)
+  return dataRecipes.value.find((recipeItem) => recipeItem.id === recipeToUpdateID)
 })
 
-function handleSubmitEdit(payload) {
-  const idToEditIndex = dataRecipes.value.findIndex(
-    (recipeItem) => recipeItem.id === recipeToEditID,
+function handleSubmitUpdate(payload) {
+  const idToUpdateIndex = dataRecipes.value.findIndex(
+    (recipeItem) => recipeItem.id === recipeToUpdateID,
   )
-  dataRecipes.value[idToEditIndex] = { id: recipeToEditID, ...payload }
+  dataRecipes.value[idToUpdateIndex] = { id: recipeToUpdateID, ...payload }
   saveAll(dataRecipes.value)
 
   router.replace('/')
